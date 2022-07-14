eb9f93b59605ef84190d73bd5482f102ea5606dd
efomc <98823444+efomc@users.noreply.github.com>
2022-07-14 21:19:21 +0300

    Add files via upload

---
+++ tetrika_test1.py
@@ -0,0 +1,13 @@
+def task(array):
+    array = str(array)
+    return array.find('0')
+
+
+def main():
+    array = '111111111111111111111111100000000'
+    print(task(array))
+
+
+
+if __name__ == "__main__":
+    main()
